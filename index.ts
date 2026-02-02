8bb40209f5420dfdb5323f297a159457bc25b2bd
Christian Cade <christainchukwunyere06@gmail.com>
2026-02-02 23:27:43 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+type User = {
+  name: string;
+  age: number;
+  skills: string[];
+};
+
+function describeUser(user: User): string {
+  return `${user.name} is ${user.age} years old and knows ${user.skills.join(", ")}`;
+}
+
+const dev: User = {
+  name: "Ifeanyi",
+  age: 25,
+  skills: ["JavaScript", "Security", "AI"],
+};
+
+console.log(describeUser(dev));
+
+
